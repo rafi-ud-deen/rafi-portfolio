8d876c421def7e94553ecdea12bc15a708d7cc82
Rafi Ud Deen <RAFIU@Machine.(none)>
2026-01-28 21:12:25 +0530

    Initial commit - Rafi Ud Deen Portfolio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import TopButton from './components/topButton/TopButton'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+      <TopButton />
+    </div>
+  )
+}
+
+export default App
